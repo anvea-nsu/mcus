dc98f5ab3adcb6d3e5c63dd4f1cfeb9f2971415c
anveretal <anveretal@mail.ru>
2026-05-21 11:38:40 +0700

    add lab 01

---
+++ labs/01-introduction-to-Microchip-Studio/main.asm
@@ -0,0 +1,21 @@
+.include "m168Adef.inc"
+
+.org 0x0000
+    rjmp MAIN
+
+MAIN:
+    ldi r16, 5
+    ldi r17, 3
+    add r16, r17
+
+    inc r16
+
+    sub r16, r17
+
+    dec r16
+
+    sts 0x0100, r16
+    lds r18, 0x0100
+
+LOOP:
+    rjmp LOOP
